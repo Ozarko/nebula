e35eaca6403835de78434f3eaf92e8312319fa66
Ozarko <ozarkonv@gmail.com>
2024-12-25 21:26:14 +0200

    style: standardize CSS variable naming and formatting in globals.css and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,31 +8,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        "secondary": "var(--secondary)",
+        secondary: "var(--secondary)",
         "text-primary": "var(--text-primary)",
         "text-secondary": "var(--text-secondary)",
-        "border": "var(--border)",
-      
+        border: "var(--border)",
       },
       backgroundImage: {
         "nebula-gradient":
           "linear-gradient(165.54deg, #141333 -33.39%, #202261 15.89%, #543C97 55.84%, #6939A2 74.96%)",
       },
       fontFamily: {
-        'open-sans': ["var(--font-open-sans)", "sans-serif"],
+        "open-sans": ["var(--font-open-sans)", "sans-serif"],
       },
       boxShadow: {
-        'primary': '2px 2px 6px 0px #543c9740'
+        primary: "2px 2px 6px 0px #543c9740",
       },
       container: {
         center: true,
-        padding: '1rem',
+        padding: "1rem",
         screens: {
-          sm: '100%',
-          md: '100%',
-          lg: '330px',
-          xl: '330px',
-          '2xl': '330px',
+          sm: "100%",
+          md: "100%",
+          lg: "330px",
+          xl: "330px",
+          "2xl": "330px",
         },
       },
     },
