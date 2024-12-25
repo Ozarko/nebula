f8e3a508782658d2bd5915367d665c583f46f98d
Ozarko <ozarkonv@gmail.com>
2024-12-25 16:10:51 +0200

    chore: update ESLint configuration with additional plugins and rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import importPlugin from "eslint-plugin-import";
+import securityPlugin from "eslint-plugin-security";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +14,39 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next", "prettier"),
+  {
+    files: ["**/*.tsx"],
+    plugins: {
+      "jsx-a11y": jsxA11y,
+      import: importPlugin,
+      security: securityPlugin,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...jsxA11y.configs.recommended.rules,
+      ...securityPlugin.configs.recommended.rules,
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            ["internal"],
+            ["parent", "sibling"],
+            "index",
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "comma-dangle": ["error", "always-multiline"],
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      "no-unused-vars": ["error"],
+    },
+  },
 ];
 
 export default eslintConfig;
