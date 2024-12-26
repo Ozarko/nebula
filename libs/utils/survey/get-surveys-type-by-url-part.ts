b5440ba62439bf35ef6d213a15b10e15f1bce644
Ozarko <ozarkonv@gmail.com>
2024-12-26 20:47:21 +0200

    style: add missing commas in survey configuration functions

--- libs/utils/survey/get-surveys-type-by-url-part.ts
+++ libs/utils/survey/get-surveys-type-by-url-part.ts
@@ -2,14 +2,14 @@ import { SurveyConfig } from "@typeslib/services/survey-config";
 
 export const getSurveysTypeByUrlPart = (
   urlPart: string,
-  configs: SurveyConfig[]
+  configs: SurveyConfig[],
 ) => {
   if (!urlPart) {
     console.error("getSurveysIdByUrlPart: urlPart is not defined");
     return null;
   }
   const surveyConfig = configs.find(
-    (config) => config.surveyUrlPart === urlPart
+    (config) => config.surveyUrlPart === urlPart,
   );
   if (!surveyConfig) {
     console.error("getSurveysIdByUrlPart: surveyConfig is not defined");
