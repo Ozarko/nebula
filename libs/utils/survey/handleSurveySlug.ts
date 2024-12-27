9438dd2551829cee07ee34e95967d9de4a2dcf73
Ozarko <ozarkonv@gmail.com>
2024-12-27 19:52:10 +0200

    fix: remove unnecessary slug check in handleSurveySlug function

--- libs/utils/survey/handleSurveySlug.ts
+++ libs/utils/survey/handleSurveySlug.ts
@@ -2,10 +2,6 @@ import { SurveyConfig } from "@typeslib/services/survey-config";
 import { getSurveysTypeByUrlPart } from "./get-surveys-type-by-url-part";
 
 export const handleSurveySlug = (slug: string[], configs: SurveyConfig[]) => {
-  if (!slug) {
-    console.log("handleSurveySlug: slug is not defined");
-    return null;
-  }
   if (!slug[0] || !slug[1]) {
     console.log("handleSurveySlug: slug[0] or slug[1] is not defined");
     return null;
