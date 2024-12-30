116fa6b8a62ba73210f35e67354628db1675bc3e
Ozarko <ozarkonv@gmail.com>
2024-12-30 17:22:51 +0200

    feat: add SpeedInsights component for performance monitoring and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "server-only";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import { MAIN_METADATA as metadata } from "@constants/metadata/root-layout";
 import openSans from "@fonts/open-sans";
 import { AppProvider } from "@providers/app-provider";
@@ -13,7 +15,10 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <AppProvider>
       <html lang="en">
-        <body className={`${openSans.variable} antialiased`}>{children}</body>
+        <body className={`${openSans.variable} antialiased`}>
+          {children}
+          <SpeedInsights />
+        </body>
       </html>
     </AppProvider>
   );
